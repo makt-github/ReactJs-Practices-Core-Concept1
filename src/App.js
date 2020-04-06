bd591b88fa0afdcdbc1b06b26f0c5bde3ded43d3
makt-github <makt.forum@gmail.com>
2020-04-06 16:37:25 +0600

    First Step

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello React <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <p> Happy Learning  React</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
